248e3a9cc36375e0c007b91d7190716adb81b314
Yundo Kim <yundokim0405@naver.com>
2020-09-06 14:44:00 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -14,13 +14,18 @@ from keras.layers import Dense
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-p', dest='path', required=True, help='File Path')
+parser.add_argument('-f', dest='file', required=True, help='File Path')
+parser.add_argument('-m', dest='model', required=False, help='Model Path')
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
 num_classes = 12
 
 input_shape = (256, 256, 1)
 
 breeds = ['Sphynx', 'Birman', 'Egyptian Mau', 'Ragdoll', 'Abyssinian', 'Siamese', 'Maine Coon', 'Bengal', 'British Shorthair', 'Bombay', 'Russian Blue', 'Persian']
+
+args = parser.parse_args()
 
 model = Sequential(
     [
@@ -37,13 +42,15 @@ model = Sequential(
 )
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.load_weights('models/save_at_40.h5')
 
-args = parser.parse_args()
+if args.model is None:
+    model.load_weights('models/save_at_50.h5')
+else:
+    model.load_weights(args.model)
 
 # img = cv2.imread('photos/Test/Persian.jpg', cv2.IMREAD_GRAYSCALE)
 try:
-    img = cv2.imread(args.path, cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(args.file, cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, dsize=input_shape[:2], interpolation=cv2.INTER_CUBIC)
     x = img.astype('float32') / 255
     x = np.expand_dims(x, -1)
